1f5a6f1d1921ea4be1db15b2853d0f5e42d387e4
Charlotte Roederer <lemaildecharlotteroederer@gmail.com>
2021-05-19 11:07:16 +0200

    name spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :watches
   namespace :my do
     resources :watches
+    resources :bookings
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
